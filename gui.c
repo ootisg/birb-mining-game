b1086b21a4375f98b0af272db6492787183f1dff
nathan <ootisg@illcomics.com>
2021-04-04 18:02:05 -0500

    Gave GUI component reigons their own image data

--- gui.c
+++ gui.c
@@ -2,7 +2,7 @@
 
 #include "object_handler.h"
 
-void init_gui_component (void* ptr, char* layout) {
+void init_gui_component (void* ptr, char* layout, rectangle* bounds, char* bg) {
 	
 	//Make the GUI component
 	gui_component* cpt = (gui_component*)ptr;
@@ -10,6 +10,23 @@ void init_gui_component (void* ptr, char* layout) {
 	//Load the layout from the file
 	json_object* layout_json = read_json_file (layout);
 	cpt->layout = make_layout (malloc (sizeof (layout_element)), layout_json);
+	cpt->reigons = get_layout_reigons (cpt->layout, make_rectangle (malloc (sizeof (rectangle)), 0, 0, 1, 1));
+	
+	//Initialize the image data
+	cpt->reigon_data = malloc (sizeof (gui_reigon_data) * cpt->reigons->size);
+	linked_list_node* curr = cpt->reigons->head;
+	int index = 0;
+	while (curr) {
+		rectangle* bounds = (rectangle*)curr->node_data;
+		printf ("HIA\n");
+		print_rectangle (bounds);
+		int buffer_width = (int)(bounds->width * TEXTURE_SIZE);
+		int buffer_height = (int)(bounds->height * TEXTURE_SIZE);
+		cpt->reigon_data[index].img_data = malloc (sizeof (int) * buffer_width * buffer_height);
+		cpt->reigon_data[index].mouse_status = 0;
+		curr = curr->next;
+		index++;
+	}
 	
 	//Initialize the game_object interface
 	cpt->ui = make_game_object (malloc (sizeof (game_object)), "gui_component");
@@ -17,6 +34,7 @@ void init_gui_component (void* ptr, char* layout) {
 	cpt->ui->game_logic_call = gui_component_frame_event;
 	cpt->ui->draw_call = gui_component_draw;
 	declare_game_object (get_global_object_handler (), cpt->ui);
+	cpt->ui->sprite = make_sprite (bg);
 	
 	//Set the event calls to NULL
 	cpt->click_event = NULL;
@@ -24,12 +42,13 @@ void init_gui_component (void* ptr, char* layout) {
 	cpt->mouse_enter_event = NULL;
 	cpt->mouse_exit_event = NULL;
 	cpt->key_event = NULL;
+	cpt->reigon_init_func = NULL;
 	cpt->render_func = NULL;
 	
 }
 
 void gui_component_init (game_object* obj) {
-	
+	default_init (obj);
 }
 
 void gui_component_frame_event (game_object* obj) {
@@ -37,7 +56,7 @@ void gui_component_frame_event (game_object* obj) {
 }
 
 void gui_component_draw (game_object* obj) {
-	
+	default_draw (obj);
 }
 
 rectangle* get_layout_elem_by_index (int index) {
--- gui.h
+++ gui.h
@@ -3,21 +3,29 @@
 
 #include "game_object.h"
 
+struct gui_reigon_data {
+	unsigned char* img_data;
+	int mouse_status;
+};
+
 struct gui_component {
 	game_object* ui;
 	layout_element* layout;
 	linked_list* reigons;
+	struct gui_reigon_data* reigon_data;
 	void (*click_event) (struct gui_component* cpt, int index, int button, float x, float y);
 	void (*hover_event) (struct gui_component* cpt, int index, float x, float y);
 	void (*mouse_enter_event) (struct gui_component* cpt, int index, float x, float y);
 	void (*mouse_exit_event) (struct gui_component* cpt, int index, float x, float y);
 	void (*key_event) (struct gui_component* cpt, int index);
-	void (*render_func) (struct gui_component* cpt);
+	void (*reigon_init_func) (struct gui_component* cpt, int index);
+	void (*render_func) (struct gui_component* cpt, int index);
 };
 
 typedef struct gui_component gui_component;
+typedef struct gui_reigon_data gui_reigon_data;
 
-void init_gui_component (void* ptr, char* layout);
+void init_gui_component (void* ptr, char* layout, rectangle* bounds, char* bg);
 void gui_component_init (game_object* obj);
 void gui_component_frame_event (game_object* obj);
 void gui_component_draw (game_object* obj);
